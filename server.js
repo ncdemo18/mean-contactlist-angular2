5d40848817d463ca344f70472a89d62a3cbe2903
Rail Khamitov <rail.khamitov@netcracker.com>
2017-10-14 20:14:03 +0400

    olol

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ var ObjectID = mongodb.ObjectID;
 var CONTACTS_COLLECTION = "contacts";
 
 var app = express();
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(bodyParser.json());
 
 // Create link to Angular build directory
