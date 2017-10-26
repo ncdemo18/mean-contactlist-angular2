b6d0ce6a18c96f974583ab950242d3b191f59bc2
Rail Khamitov <rail.khamitov@netcracker.com>
2017-10-26 19:11:06 +0400

    add get dashboard info

--- server.js
+++ server.js
@@ -144,18 +144,9 @@ app.get("/api/dashboard/next", function (req, res) {
 
 
 app.get("/api/dashboard/next/push", function (req, res) {
-  db.collection("pages").findOne({"key": "yes"}, function (err, doc) {
+  db.collection("pages").findOneAndUpdate({_id: doc._id}, {"key": "no"}, function (err, docUpdate) {
     if (err) {
-      handleError(res, err.message, "Key equals NO");
-    } else {
-      // if (doc.key === "yes") {
-      db.collection("pages").findOneAndUpdate({_id: doc._id}, {"key": "no"}, function (err, docUpdate) {
-        if (err) {
-          handleError(res, err.message, "Failed to create new key.");
-        }
-      });
-      // }
-      res.status(200).json(doc);
+      handleError(res, err.message, "Failed to create new key.");
     }
   });
 });
